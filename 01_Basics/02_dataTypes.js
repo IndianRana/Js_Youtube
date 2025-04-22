68933974c5bfb1e393a6f227feceb78acdb6e58e
IndianRana <indianrana007@gmail.com>
2025-04-23 00:18:28 +0530

    update:- dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict";  //treat all Js code new version
+
+//alert(3 + 3)  //we are use Node Js not Browser
+
+console.log(3 +
+     3);        //not prefer
+
+console.log("Aditya"); // code readability should be high
+
+let name = "Indin Rana"
+let age = 22
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+//bigint => extra large no.
+//string => "" ,inside this
+//bollean => true/false
+//null => standalone value, type ->Object
+//undefined => undefined
+//symbol=>
+//Object =>
+
+    console.log(typeof "Indian Rana");  //string
+    console.log(typeof age);   //number
+    console.log(typeof null); //object
+    
+    
