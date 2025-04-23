c7c9c024368b91fde2afbcae20e2973472fd2b3e
IndianRana <indianrana007@gmail.com>
2025-04-23 18:09:51 +0530

    comparison

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,16 @@
+//console.log(2 > 1);
+//console.log(2 < 1);
+//console.log(2 >= 1);
+//console.log(2 <= 1);
+//console.log(2 == 1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+console.log("" > 1);
+
+console.log(null <= 0);  //Avoid these comparison
+console.log(undefined == 0);
+
+/// === for strict check
+
+console.log("2" === 1);
