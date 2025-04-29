c4a9d4be33c8b2f21f0d6c6f793837aecb7176a1
IndianRana <indianrana007@gmail.com>
2025-04-29 22:42:32 +0530

    Date in Js

---
+++ 01_Basics/07_date.injs.js
@@ -0,0 +1,25 @@
+let myDate = new Date()
+console.log(typeof myDate.toDateString());   // convert in string
+
+console.log( myDate.toLocaleString());   // convert 29/04/2025, 18:26:16
+
+console.log( myDate.toISOString());   // convert 2025-04-29T12:56:16.233Z
+
+console.log( myDate.toJSON());   // convert 2025-04-29T12:56:16.233Z
+
+console.log(typeof myDate);   // date is Object
+
+let mycreatDate = new Date(2023,0,20)
+console.log(mycreatDate.toLocaleString());  //20/01/2023, 00:00:00
+
+//let myTimeStemp = now Date()
+
+//console.log(myTimeStemp.getMonth());
+
+//Work is panding
+
+
+
+
+
+
