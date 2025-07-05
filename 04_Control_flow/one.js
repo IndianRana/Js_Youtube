59fbc80a2020113102898dcbe494f505b9c491e6
IndianRana <indianrana007@gmail.com>
2025-07-05 16:04:33 +0530

    control flow

---
+++ 04_Control_flow/one.js
@@ -0,0 +1,57 @@
+//if
+const isUserLoggedIn = true
+const temperature = 41
+
+// if ( temperature === 40){
+//     console.log("temprature is less then 50");
+// }else{
+//     console.log("tempraturis greater then 50");
+// }
+
+// console.log("executed");
+ 
+
+// const score = 200
+
+// if(score > 100){
+//     let power = "fly"
+//     console.log(`User power:${power}`);
+    
+// }
+// console.log(`User power:${power}`);
+
+const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2")
+// //note use this format
+
+// if(balance < 500){
+//     console.log("less then 500" );
+    
+// }else if(balance <750){
+//     console.log("less then 750");
+    
+// }else if(balance <900){
+//     console.log("less then 900");
+    
+// }else{
+//     console.log("less then 1200");
+    
+// }
+
+
+const userLoggedIn = true
+const debitcard = true
+const loggedinFromGoogle = false
+const loggedinFromEmil = true
+
+if(userLoggedIn && debitcard && 2==2){
+    console.log("Allow to buy course");
+}
+    
+if(loggedinFromGoogle || loggedinFromEmil ){
+    console.log("user log in")
+    
+    
+}
+    
