a96d7a7a37919a2642027ec2c18f0842742446f5
IndianRana <indianrana007@gmail.com>
2025-07-12 15:03:59 +0530

    forof

---
+++ 05_iteration/three.js
@@ -0,0 +1,41 @@
+//for
+
+// [", "","",]
+// {}, {}, {},]
+
+const arr = [1,2,3,4,5]
+for (const num of arr){
+    // console.log(num)
+    
+}
+
+const greetings = "hello world"
+for (const greet of greetings){
+    // console.log(greet)
+    
+    
+}
+
+//Map
+
+const map = new Map()
+map.set('IN', "india")
+map.set('USA', "United state of india")
+map.set('FN', "France")
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);
+    
+} 
+
+const myObject = {
+    'game1':  "NFS",
+    'game2':  "Spiderman"
+}
+
+// for (const [key,value] of myObject) {
+//     console.log(key, ':-', value);
+    
+// }
