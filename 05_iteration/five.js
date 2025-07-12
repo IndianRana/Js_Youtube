3592f59e75463197469e4105745e81fbb4b4c0f1
IndianRana <indianrana007@gmail.com>
2025-07-12 15:34:38 +0530

    ForEach Loop

---
+++ 05_iteration/five.js
@@ -0,0 +1,42 @@
+const coding = ["jds", "ruby", "c++", "python", "java", "cpp"]
+
+// coding.forEach( function (item) {
+//     console.log(item);
+    
+// })
+
+// coding.forEach( (item) => {
+//     console.log(item);
+    
+// })
+
+// function printMe(item) {
+//     console.log(item);
+    
+// }
+// coding.forEach(printMe)
+
+// coding.forEach( (item, index, arr) => {
+//     console.log(item,index,arr);
+    
+// });
+
+const myCoding = [
+    {
+        languageName: "javscript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "python"
+    }
+]
+
+myCoding.forEach( (item) => {
+    console.log(item.languageName);
+    
+})
